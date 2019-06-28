c8431f624db013d52f181ec53ed8a19f241916dd
ganzourii <ganzouri97@gmail.com>
2019-06-28 19:12:59 +0200

    Create DijkstraAlgorithm.py

---
+++ Algorithms/DijkstraAlgorithm.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+def	dijkstra(node):
+
+	dist = np.full(vertices_num,'inf')
+	parent= np.full(vertices_num,-1)
+	nodes_queue= [{'src':node ,'dest':node,'weight':0}]
+	
+	while len(nodes_queue) != 0:
+		current_node=nodes_queue[0]['src']
+		prev_node=nodes_queue[0]['dest']
+		cur_dist=nodes_queue[0]['weight']
+		nodes_queue.pop(0)
+		if dist[current_node] != 'inf':
+			continue
+		dist[current_node]=cur_dist
+		parent[current_node]=prev_node	
+		
+		for i in adj_list[current_node]:
+			distance=cur_dist+adj_list[current_node][i]
+			temp={'src':i ,'dest':current_node,'weight':distance}
+			if dist[i] !='inf':
+				continue
+			nodes_queue.append(temp.copy())
+			
+		nodes_queue.sort(key=lambda i: i['weight'])
+	
+	return dist,parent
