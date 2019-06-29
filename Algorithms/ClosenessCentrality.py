859a1ce07e6b23f841ba6c7289d133b73a4e61c0
Salma Tarek <1500667@eng.asu.edu.eg>
2019-06-29 20:19:49 +0200

    Update ClosenessCentrality.py

--- Algorithms/ClosenessCentrality.py
+++ Algorithms/ClosenessCentrality.py
@@ -1,11 +1,7 @@
-#Closeness Centrality
 closeness_centrality = {}
 for i in range(vertices_num):
-	distance_sum = 0
 	distance,parent = dijkstra (i)
-	for x in range(vertices_num):
-		distance_sum += int(distance[x])
-	closeness_centrality[i] = (vertices_num-1)/distance_sum
-
-for i in range(vertices_num):
-	print(closeness_centrality[i])
+	distance_sum = sum(distance)
+	closeness_centrality[i]=(vertices_num-1)/distance_sum
+	print('%0.12f' % closeness_centrality[i])
+	
